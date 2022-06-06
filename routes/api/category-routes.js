9c29bd24f7940bc6cf71f6ea8c8bb647497e987f
Sterling Peck <peck.sterling@gmail.com>
2022-06-06 09:57:17 -0600

    cat routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,108 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
+// find all categories
+// be sure to include its associated Products
 router.get("/", (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    // Include associated products
+    include: {
+      model: Product,
+    },
+  })
+    .then((dbCategoryInfo) => res.json(dbCategoryInfo))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// find one category by its `id` value
+// be sure to include its associated Products
 router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    // Include associated products
+    include: {
+      model: Product,
+    },
+  })
+    .then((dbCategoryInfo) => {
+      if (!dbCategoryInfo) {
+        res.status(404).json({
+          message: "This product ID does not exist",
+        });
+        return;
+      }
+      res.json(dbCategoryInfo);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// create a new category
 router.post("/", (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbCategoryInfo) => res.json(dbCategoryInfo))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// update a category by its `id` value
 router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbCategoryInfo) => {
+      if (!dbCategoryInfo) {
+        res.status(404).json({
+          message: "This category ID does not exist",
+        });
+        return;
+      }
+      res.json(dbCategoryInfo);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// delete a category by its `id` value
 router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryInfo) => {
+      if (!dbCategoryInfo) {
+        res.status(404).json({
+          message: "This category ID does not exist",
+        });
+        return;
+      }
+      res.json(dbCategoryInfo);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
